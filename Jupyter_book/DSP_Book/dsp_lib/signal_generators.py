2874a5bca73cad9bb499ee73d0c11742a8c7a608
Kalashnikov_N <kalashnikov@form.ru>
2025-08-18 16:36:06 +0300

    добавил коменты

--- Jupyter_book/DSP_Book/dsp_lib/signal_generators.py
+++ Jupyter_book/DSP_Book/dsp_lib/signal_generators.py
@@ -1,10 +1,14 @@
 # signal_generators.py
 import numpy as np
+# 2 * np.pi - можно представить как 360°. 
+# 2 * np.pi / frequency - 360°/количество частей одного периода
+# n - число частей одного периода
+# phase - смещение всех значений на угол
 
 def gen_sin(amplitude: float, frequency: float, phase: float, n_samples: int) -> np.ndarray:
     n = np.arange(n_samples)
-    return amplitude * np.sin(2 * np.pi * (1/frequency) * n + phase)
+    return amplitude * np.sin(2 * np.pi / frequency * n + phase)
 
 def gen_cos(amplitude: float, frequency: float, phase: float, n_samples: int) -> np.ndarray:
     n = np.arange(n_samples)
-    return amplitude * np.cos(2 * np.pi * (1/frequency) * n + phase)
+    return amplitude * np.cos(2 * np.pi / frequency * n + phase)
